a33826f1dd713295e0ceaeb913baed3b8ea25d17
mohammadreza <mghu.ir@gmail.com>
2025-02-10 02:58:06 -0800

    Updated project structure and files2

--- src/Buton.jsx
+++ src/Buton.jsx
@@ -1,6 +1,6 @@
 function Buton(props) {
   return (
-    <button onClick={props.onclick}>{props.children}</button>
+    <button id="button" onClick={props.onclick}>{props.children}</button>
   )
 }
 export default Buton
